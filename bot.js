1dfe5419df92f7a9d2a379a1fa0f50561ed6a4e0
xDOASx <xdoasx@github.com>
2021-04-07 17:13:53 -0700

    Define configuration options.

--- bot.js
+++ bot.js
@@ -1,1 +1,13 @@
 const tmi = require('tmi.js');
+import { config } from './config';
+
+// Define configuration options
+const opts = {
+    identity : {
+        username: config.BOT_USERNAME,
+        password: config.OAUTH_TOKEN
+    },
+    channels : [
+        config.CHANNEL_NAME
+    ]
+};
